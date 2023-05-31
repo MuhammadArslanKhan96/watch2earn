cf7765697d2d65a1c92a06bb10fc6bf9d7217a0b
Muhammad Arslan <98827339+MuhammadArslanKhan96@users.noreply.github.com>
2023-05-31 18:47:19 +0500

    Update AI.js

--- pages/AI.js
+++ pages/AI.js
@@ -21,7 +21,7 @@ const AI = () => {
 
     function handleSelectLanguage() {
         const select = document.getElementsByTagName("select")[0].value;
-        fetch(`http://ec2-52-23-228-216.compute-1.amazonaws.com/?language=${select}`)
+        fetch(`https://d125-52-23-228-216.ngrok-free.app/?language=${select}`)
             .then(async function res(res) {
                 data = await res.json();
             })
@@ -68,7 +68,7 @@ const AI = () => {
         console.log(text, voice);
         if (text === "") return;
         else {
-            fetch(`http://ec2-52-23-228-216.compute-1.amazonaws.com/?language=null&text=${text}&voice=${voice}`)
+            fetch(`https://d125-52-23-228-216.ngrok-free.app/?language=null&text=${text}&voice=${voice}`)
                 .then(async function res(res) {
                     data = await res.blob();
                     // console.log(await res.blob())
